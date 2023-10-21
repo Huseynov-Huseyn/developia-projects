ea3d35e4bd9a30fe5f9954a07a2c1bec4043d49e
Huseynhuseynov2005 <148152665+Huseynhuseynov2005@users.noreply.github.com>
2023-10-21 22:02:02 +0400

    test1

---
+++ codes/javase/src/javase/Main.java
@@ -0,0 +1,16 @@
+package javase;
+
+public class Main {
+
+	public static void main(String[] args) {
+		 int a=9;
+		 int b=10;
+		 byte c=(byte)10;
+		
+		 System.out.println(c);
+		 
+		 
+
+	}
+
+}
