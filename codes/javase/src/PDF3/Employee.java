cb774e88a9fc861aadcc6ac8dcbacd30f40bf363
Huseynhuseynov2005 <148152665+Huseynhuseynov2005@users.noreply.github.com>
2023-10-23 19:14:56 +0400

    ders5 ev isi
    class

---
+++ codes/javase/src/PDF3/Employee.java
@@ -0,0 +1,25 @@
+package PDF3;
+
+public class Employee {
+	int id;
+	String name;
+	String surname;
+	String phone;
+	String address;
+	int salary;
+	
+	public Employee() {
+		
+	}
+	
+	public Employee(String name){
+		
+	}
+	
+	public Employee(String name, String surname){
+		
+	}
+	public Employee(String name, String phone, int salary){
+		
+	}
+}
