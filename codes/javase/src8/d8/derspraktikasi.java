948b9b79d122f53f54c7835ddc8585ed5804477e
Huseynhuseynov2005 <148152665+Huseynhuseynov2005@users.noreply.github.com>
2023-11-06 19:05:10 +0400

    derspraktikasi1
    random methodu ile deyerler menimsedib onlarin ededi ortasini toplamini ve tek olanlarini cap etmek

---
+++ codes/javase/src8/d8/derspraktikasi.java
@@ -0,0 +1,27 @@
+package d8;
+
+import java.util.Random;
+
+public class derspraktikasi {
+	public static void main(String[] args) {
+		int[] arr=new int[3];
+		
+		Random rastgele = new Random();
+		int toplam=0;
+		for(int i=0;arr.length>i;i++) {
+			arr[i] =rastgele.nextInt(10);
+			System.out.println(arr[i]);
+			toplam+=arr[i];
+		}
+		System.out.println("sizin massivinizin toplam deyeri: "+toplam);
+		System.out.println("sizin massivinizin edediortasi: "+(double)toplam/arr.length);
+		System.out.println("-----");
+		
+		System.out.println("Massivini icinde olan tek ededler: ");
+		for(int i=0;arr.length>i;i++) {
+			if(arr[i]%2==1) {
+				System.out.println(arr[i]);
+			}
+		}
+	}
+}
